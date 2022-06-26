f2a365b99c139f4972a22a5ac0097bea90c73a0e
Syukron <syukronrifai@gmail.com>
2022-06-26 23:02:49 +0700

    second chapter

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -3,12 +3,26 @@
 
 use core::panic::PanicInfo;
 
+// Panic handler requires standard library specific to OS
+// which we does not provide.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop{}
+    loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
+// Let's defined our own start point instead provided by
+// the linker.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
